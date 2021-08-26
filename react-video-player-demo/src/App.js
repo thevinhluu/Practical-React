81ccd2019a1a072d7e8cae424200211ff1832f2e
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-26 17:08:42 +0700

    Video Player

---
+++ react-video-player-demo/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<ReactPlayer
+				width='480px'
+				height='240px'
+				controls
+				url='https://youtu.be/7sDY4m8KNLc'
+				onReady={() => console.log('onReady callback')}
+				onStart={() => console.log('onStart callback')}
+				onPause={() => console.log('onPause callback')}
+				onEnded={() => console.log('onEnded callback')}
+				onError={() => console.log('onError callback')}
+			/>
+		</div>
+	);
+}
+
+export default App;
